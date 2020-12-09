ef92fdfaf3c47a1336c72744cd13fcc82f6ed041
kompir <aalexandyr@gmail.com>
2020-12-09 16:15:32 +0200

    -- Add first version.

---
+++ server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kompir/go-gin-aws/controller"
+	"github.com/kompir/go-gin-aws/middlewares"
+	"github.com/kompir/go-gin-awsc/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("/var/log/golang/gin-app.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger())
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	// Basic Authorization Middleware applies to "/api" only.
+	apiRoutes := server.Group("/api", middlewares.BasicAuth())
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+			}
+
+		})
+	}
+
+	// The "/view" endpoints are public (no Authorization required)
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
+	// We can setup this env variable from the EB console
+	port := os.Getenv("PORT")
+	// Elastic Beanstalk forwards requests to port 5000
+	if port == "" {
+		port = "5000"
+	}
+	server.Run(":" + port)
+}
